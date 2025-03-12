953de5cdff0fd4053b6788ba58763d637141771f
Greentaylor27 <greentaylor18@gmail.com>
2025-03-12 11:38:45 -0500

    Updating package.json and package-lock.json
    Updating schema with dummy data and graphiql2 is my graphql query

--- GraphQL_API/server/schema/schema.js
+++ GraphQL_API/server/schema/schema.js
@@ -1,4 +1,5 @@
 const { GraphQLObjectType ,GraphQLString, GraphQLInt, GraphQLSchema } = require('graphql');
+const lodash = require('lodash');
 
 const TaskType = new GraphQLObjectType({
     name: "Task",
@@ -17,13 +18,27 @@ const RootQuery = new GraphQLObjectType({
             type: TaskType,
             args: { id: { type: GraphQLString } },
             resolve(parent, args) {
-                return {};
+                return lodash.find(tasks, {id: args.id});
             }
         }
     }
-})
+});
 
+const tasks = [
+    {
+        id: "1",
+        title: "Create your first webpage",
+        weight: 1,
+        description: "Create your first HTML file 0-index.html with:\n- Add the doctype on the first line (without any comment)\n- After the doctype, open and close an HTML tag\n- Open your file in your browser (the page should be blank)"
+    },
+    {
+        id: "2",
+        title: "Structure your webpage",
+        weight: 1,
+        description: "Copy the content of 0-index.html into 1-index.html Create the head and body sections inside the html tag, create the head and body tags (empty) in this order"
+    }
+]
 
-module.exports == new GraphQLSchema({
+module.exports = new GraphQLSchema({
     query: RootQuery
-})
+});
